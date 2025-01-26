2beaba9f0244512643c97248001a93cff4a2602b
Ella Ekström <eltsu.ekstrom@gmail.com>
2025-01-26 23:29:03 +0200

    feat: html, application, student.java

--- Viikko2/handlinglists/src/main/java/hh/backend/handlinglists/domain/Student.java
+++ Viikko2/handlinglists/src/main/java/hh/backend/handlinglists/domain/Student.java
@@ -2,4 +2,41 @@ package hh.backend.handlinglists.domain;
 
 public class Student {
 
+    private String firstName;
+    private String lastName;
+
+
+    @Override
+    public String toString() {
+        return "Student [firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+
+    public Student(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public Student() {
+        this.firstName = null;
+        this.lastName = null;
+    }
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    
+
+    
 }
--- Viikko2/handlinglists/src/main/java/hh/backend/handlinglists/web/StudentController.java
+++ Viikko2/handlinglists/src/main/java/hh/backend/handlinglists/web/StudentController.java
@@ -1,8 +1,24 @@
 package hh.backend.handlinglists.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.backend.handlinglists.domain.Student;
 
 @Controller
 public class StudentController {
 
+    @GetMapping("/hello")
+    public String getStudents(Model model) {
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student("Kate", "Cole"));
+        students.add(new Student("Dan", "Brown"));
+        students.add(new Student("Mike", "Mars"));
+        model.addAttribute("students", students);
+        return "studentlist";
+    }
 }
