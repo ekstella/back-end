14132484081f91a6e9b97386efb58829264d9c99
Ella Ekström <eltsu.ekstrom@gmail.com>
2025-01-27 11:26:07 +0200

    fix: updated controller and html

--- Viikko2/hellothymeleaf/src/main/java/hh/backend/hellotyhmeleaf/webcontrol/HelloController.java
+++ Viikko2/hellothymeleaf/src/main/java/hh/backend/hellotyhmeleaf/webcontrol/HelloController.java
@@ -1,16 +1,21 @@
 package hh.backend.hellotyhmeleaf.webcontrol;
 
 import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String sayGreetings(@RequestParam String name, Model model ) {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayGreetings(@RequestParam(name = "name") String name, @RequestParam(name = 
+    "age") int age, Model model ) {
         model.addAttribute("name", name);
+        model.addAttribute("age", age);
         return "hello";
     }
 }
