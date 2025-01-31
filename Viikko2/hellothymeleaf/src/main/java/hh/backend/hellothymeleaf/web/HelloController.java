52f5dfd4f73918aeafae4846f7cf9661296be73c
Ella Ekström <eltsu.ekstrom@gmail.com>
2025-01-31 19:30:14 +0200

    fix: typo

--- Viikko2/hellothymeleaf/src/main/java/hh/backend/hellothymeleaf/web/HelloController.java
+++ Viikko2/hellothymeleaf/src/main/java/hh/backend/hellothymeleaf/web/HelloController.java
@@ -1,4 +1,4 @@
-package hh.backend.hellotyhmeleaf.webcontrol;
+package hh.backend.hellothymeleaf.web;
 
 import org.springframework.stereotype.Controller;
 // import org.springframework.web.bind.annotation.GetMapping;
