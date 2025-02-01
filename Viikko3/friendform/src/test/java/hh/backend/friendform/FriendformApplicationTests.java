e5ae591ffa77f042dfd6eeb7460fa31dfd2a7c58
Ella Ekström <eltsu.ekstrom@gmail.com>
2025-02-01 19:10:29 +0200

    feat: create friendform-project

---
+++ Viikko3/friendform/src/test/java/hh/backend/friendform/FriendformApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.backend.friendform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
